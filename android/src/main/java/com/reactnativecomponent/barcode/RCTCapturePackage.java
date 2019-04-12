a947fbf440db1bd60755ed477477177b71bdeaea
tzraeq <tzraeq@163.com>
2019-04-12 13:05:48 +0800

    去掉一个override，兼容高低版本的rn

--- android/src/main/java/com/reactnativecomponent/barcode/RCTCapturePackage.java
+++ android/src/main/java/com/reactnativecomponent/barcode/RCTCapturePackage.java
@@ -31,24 +31,24 @@ public class RCTCapturePackage implements ReactPackage {
 
 
     @Override
-        public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
-             mModuleInstance = new RCTCaptureModule(reactApplicationContext,captureManager);
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
+        mModuleInstance = new RCTCaptureModule(reactApplicationContext,captureManager);
         return Arrays.<NativeModule>asList(
                 mModuleInstance
         );
-        }
+    }
 
-        @Override
-        public List<Class<? extends JavaScriptModule>> createJSModules() {
-            return Collections.emptyList();
-        }
+    // This method is kept for RN < 0.47 compatibility
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
 
-        @Override
-        public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
-            //noinspection ArraysAsListWithZeroOrOneArgument
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
+        //noinspection ArraysAsListWithZeroOrOneArgument
 
 //            return Arrays.<ViewManager>asList(captureManager,linearGradientViewManager);
-            return Arrays.<ViewManager>asList(captureManager);
-        }
+        return Arrays.<ViewManager>asList(captureManager);
+    }
 
     }
