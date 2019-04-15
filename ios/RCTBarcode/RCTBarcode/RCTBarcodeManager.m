698d4de31cb7b3757cbccc10bd8f4e956b2f10b2
tzraeq <tzraeq@163.com>
2019-04-15 17:48:26 +0800

    为IOS增加扫描本地二维码图片的方法

--- ios/RCTBarcode/RCTBarcode/RCTBarcodeManager.m
+++ ios/RCTBarcode/RCTBarcode/RCTBarcodeManager.m
@@ -2,6 +2,8 @@
 #import "RCTBarcode.h"
 #import "RCTBarcodeManager.h"
 
+#import <AVFoundation/AVFoundation.h>
+#import <CoreImage/CoreImage.h>
 
 
 @interface RCTBarcodeManager ()
@@ -296,5 +298,43 @@ RCT_EXPORT_METHOD(stopSession) {
             };
 }
 
+RCT_EXPORT_METHOD(readerQR:(NSString *)fileUrl resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject){
+  dispatch_sync(dispatch_get_main_queue(), ^{
+    NSString *result = [self readerQR:fileUrl];
+    if(result){
+      resolve(result);
+    }else{
+      /*NSString *domain = @"yitang.xiao";
+      NSString *desc = NSLocalizedString(@"没有相关二维码", @"");
+      NSDictionary *userInfo = @{ NSLocalizedDescriptionKey : desc };
+      NSError *error = [NSError errorWithDomain:domain
+                                           code:404
+                                       userInfo:userInfo];*/
+      reject(@"404",@"No QR Code found",nil);
+    }
+  });
+  
+  
+  
+}
+
+-(NSString*)readerQR:(NSString*)fileUrl{
+  fileUrl = [fileUrl stringByReplacingOccurrencesOfString:@"file://" withString:@""];
+  
+  CIContext *context = [CIContext contextWithOptions:nil];
+  
+  // CIDetector(CIDetector可用于人脸识别)进行图片解析，声明一个CIDetector，并设定识别类型 CIDetectorTypeQRCode
+  CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeQRCode context:context options:@{CIDetectorAccuracy:CIDetectorAccuracyHigh}];
+  NSData *fileData = [[NSData alloc] initWithContentsOfFile:fileUrl];
+  CIImage *ciImage = [CIImage imageWithData:fileData];
+  NSArray *features = [detector featuresInImage:ciImage];
+  if(!features || features.count==0){
+    return nil;
+  }
+  //3. 获取扫描结果
+  CIQRCodeFeature *feature = [features objectAtIndex:0];
+  NSString *scannedResult = feature.messageString;
+  return scannedResult;
+}
 
 @end
